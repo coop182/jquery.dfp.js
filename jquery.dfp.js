84d97ea82653a062c02a11eb139e9303e7a7b4d4
Matt Cooper <matt@matthewcooper.net>
2012-11-20 10:16:01 +0000

    Minor formatting changes to be jslint compatible.

--- jquery.dfp.js
+++ jquery.dfp.js
@@ -5,7 +5,7 @@
  * Copyright 2012 Matt Cooper
  * Released under the MIT license
  */
-(function($,window) {
+(function ($, window) {
 
     "use strict";
 
@@ -30,13 +30,13 @@
      * @param  String selector The adunit selector
      * @param  Object options  Custom options to apply
      */
-    var init = function(id,selector,options) {
+    var init = function (id, selector, options) {
 
         dfpID = id || dfpID;
         dfpSelector = selector || dfpSelector;
         options = options || {};
         dfpLoader();
-        $(function(){createAds(options);});
+        $(function () { createAds(options); });
 
     };
 
@@ -44,7 +44,7 @@
      * Main function to find and create all Ads
      * @param Object options Custom options to apply
      */
-    var createAds = function(options) {
+    var createAds = function (options) {
 
         // Array to Store AdUnits
         var adUnitArray = [];
@@ -53,18 +53,18 @@
         var URLTargets = getURLTargets();
 
         var dfpOptions = {
-            'setTargeting':{
-                'inURL':URLTargets.inURL,
-                'URLIs':URLTargets.URLIs,
-                'Query':URLTargets.Query,
-                'Domain':window.location.host
+            'setTargeting': {
+                'inURL': URLTargets.inURL,
+                'URLIs': URLTargets.URLIs,
+                'Query': URLTargets.Query,
+                'Domain': window.location.host
             },
-            'enableSingleRequest':true,
-            'collapseEmptyDivs':'original'
+            'enableSingleRequest': true,
+            'collapseEmptyDivs': 'original'
         };
 
         // Make sure the default setTargeting is not lost in the object merge
-        if(typeof options.setTargeting !== 'undefined' && typeof dfpOptions.setTargeting !== 'undefined') {
+        if (typeof options.setTargeting !== 'undefined' && typeof dfpOptions.setTargeting !== 'undefined') {
             options.setTargeting = $.extend(options.setTargeting, dfpOptions.setTargeting);
         }
 
@@ -72,7 +72,7 @@
         dfpOptions = $.extend(dfpOptions, options);
 
         // Loops through on page Ad units and gets ads for them.
-        $(dfpSelector).each(function() {
+        $(dfpSelector).each(function () {
 
             var adUnit = this;
 
@@ -82,7 +82,7 @@
             var adUnitName = getName(adUnit);
 
             // adUnit id - this will use an existing id or an auto generated one.
-            var adUnitID = getID(adUnit,adUnitName,count);
+            var adUnitID = getID(adUnit, adUnitName, count);
 
             // get dimensions of the adUnit
             var dimensions = getDimensions(adUnit);
@@ -97,15 +97,15 @@
             adUnitArray.push(adUnitID);
 
             // Push commands to DFP to create ads
-            window.googletag.cmd.push(function() {
+            window.googletag.cmd.push(function () {
 
                 // Create the ad
-                var googleAdUnit = window.googletag.defineSlot('/'+dfpID+'/'+adUnitName, [dimensions.width, dimensions.height], adUnitID).addService(window.googletag.pubads());
+                var googleAdUnit = window.googletag.defineSlot('/' + dfpID + '/' + adUnitName, [dimensions.width, dimensions.height], adUnitID).addService(window.googletag.pubads());
 
                 // The following hijacks an internal google method to check if the div has been
                 // collapsed after the ad has been attempted to be loaded.
                 googleAdUnit.oldRenderEnded = googleAdUnit.renderEnded;
-                googleAdUnit.renderEnded = function() {
+                googleAdUnit.renderEnded = function () {
 
                     rendered++;
 
@@ -113,23 +113,23 @@
 
                     // if the div has been collapsed but there was existing content expand the
                     // div and reinsert the existing content.
-                    if(display === 'none' && $.trim($existingContent).length > 0 && dfpOptions.collapseEmptyDivs === 'original') {
+                    if (display === 'none' && $.trim($existingContent).length > 0 && dfpOptions.collapseEmptyDivs === 'original') {
                         $(adUnit).show().html($existingContent);
                         display = 'block display-original';
                     }
 
-                    $(adUnit).removeClass('display-none').addClass('display-'+display);
+                    $(adUnit).removeClass('display-none').addClass('display-' + display);
 
                     googleAdUnit.oldRenderEnded();
 
-                    // Excute afterEachAdLoaded
-                    if(typeof dfpOptions.afterEachAdLoaded === 'function') {
-                        dfpOptions.afterEachAdLoaded.call(this,adUnit);
+                    // Excute afterEachAdLoaded callback if provided
+                    if (typeof dfpOptions.afterEachAdLoaded === 'function') {
+                        dfpOptions.afterEachAdLoaded.call(this, adUnit);
                     }
 
-                    // Excute afterAllAdsLoaded
-                    if(typeof dfpOptions.afterAllAdsLoaded === 'function' && rendered === count) {
-                        dfpOptions.afterAllAdsLoaded.call(this,$(dfpSelector));
+                    // Excute afterAllAdsLoaded callback if provided
+                    if (typeof dfpOptions.afterAllAdsLoaded === 'function' && rendered === count) {
+                        dfpOptions.afterAllAdsLoaded.call(this, $(dfpSelector));
                     }
 
                 };
@@ -139,15 +139,15 @@
         });
 
         // Push DFP config options
-        window.googletag.cmd.push(function() {
+        window.googletag.cmd.push(function () {
 
-            if(dfpOptions.enableSingleRequest === true) {
+            if (dfpOptions.enableSingleRequest === true) {
                 window.googletag.pubads().enableSingleRequest();
             }
-            $.each(dfpOptions.setTargeting, function(k,v) {
-                window.googletag.pubads().setTargeting(k,v);
+            $.each(dfpOptions.setTargeting, function (k, v) {
+                window.googletag.pubads().setTargeting(k, v);
             });
-            if(dfpOptions.collapseEmptyDivs === true || dfpOptions.collapseEmptyDivs === 'original') {
+            if (dfpOptions.collapseEmptyDivs === true || dfpOptions.collapseEmptyDivs === 'original') {
                 window.googletag.pubads().collapseEmptyDivs();
             }
             window.googletag.enableServices();
@@ -155,9 +155,9 @@
         });
 
         // Display each ad
-        $.each(adUnitArray, function(k,v) {
+        $.each(adUnitArray, function (k, v) {
 
-            window.googletag.cmd.push(function(){window.googletag.display(v);});
+            window.googletag.cmd.push(function () { window.googletag.display(v); });
 
         });
 
@@ -167,27 +167,27 @@
      * Create an array of paths so that we can target DFP ads to Page URI's
      * @return Array an array of URL parts that can be targeted.
      */
-    var getURLTargets = function() {
+    var getURLTargets = function () {
 
         // Get the paths for targeting against
-        var paths = window.location.pathname.replace(/\/$/,'');
-        var patt = new RegExp('\/([^\/]*)','ig');
+        var paths = window.location.pathname.replace(/\/$/, '');
+        var patt = new RegExp('\/([^\/]*)', 'ig');
         var pathsMatches = paths.match(patt);
         var targetPaths = ['/'];
         var longestpath = '';
-        if(paths !== '/' && pathsMatches !== null) {
+        if (paths !== '/' && pathsMatches !== null) {
             var target = '';
             var size = pathsMatches.length;
-            if(size > 0) {
-                for(var i = 0; i < size; i++) {
+            if (size > 0) {
+                for (var i = 0; i < size; i++) {
                     target = pathsMatches[i];
                     targetPaths.push(target);
-                    for(var j = i+1; j < size; j++) {
+                    for (var j = i + 1; j < size; j++) {
                         target += pathsMatches[j];
                         targetPaths.push(target);
                     }
-                    if(i===0) {
-                        targetPaths.splice(-1,1);
+                    if (i === 0) {
+                        targetPaths.splice(-1, 1);
                         longestpath = target;
                     }
                 }
@@ -198,13 +198,13 @@
         targetPaths = targetPaths.reverse();
 
         // Get the query params for targeting against
-        var url = window.location.toString().replace(/\=/ig,':').match(/\?(.+)$/);
+        var url = window.location.toString().replace(/\=/ig, ':').match(/\?(.+)$/);
         var params = RegExp.$1.split("&");
 
         return {
-            'inURL':targetPaths,
-            'URLIs':targetPaths[0],
-            'Query':params
+            'inURL': targetPaths,
+            'URLIs': targetPaths[0],
+            'Query': params
         };
 
     };
@@ -216,10 +216,10 @@
      * @param  Integer count     The current count of adunit, for uniqueness
      * @return String             The ID of the adunit or a unique autogenerated ID
      */
-    var getID = function (adUnit,adUnitName,count) {
+    var getID = function (adUnit, adUnitName, count) {
 
         var id = adUnit.id;
-        if(id === '')
+        if (id === '')
         {
             id = adUnit.id = adUnitName + '-auto-gen-id-' + count;
         }
@@ -234,10 +234,10 @@
      * @param  Object adUnit The adunit to work with
      * @return String        The name of the adunit, will be the same as inside DFP
      */
-    var getName = function(adUnit) {
+    var getName = function (adUnit) {
 
         var name = adUnit.id;
-        if(typeof $(adUnit).data('adunit') !== 'undefined') {
+        if (typeof $(adUnit).data('adunit') !== 'undefined') {
 
             name = $(adUnit).data('adunit');
 
@@ -253,17 +253,17 @@
      * @param  Object adUnit The adunit to work with
      * @return Array         The dimensions of the adunit (width, height)
      */
-    var getDimensions = function(adUnit) {
+    var getDimensions = function (adUnit) {
 
         var width = $(adUnit).width();
         var height = $(adUnit).height();
 
         // check if dimensions are hardcoded and overide the size
-        if(typeof $(adUnit).data('dimensions') !== 'undefined') {
+        if (typeof $(adUnit).data('dimensions') !== 'undefined') {
 
             var dimensions = $(adUnit).data('dimensions').split('x');
-            width = parseInt(dimensions[0],10);
-            height = parseInt(dimensions[1],10);
+            width = parseInt(dimensions[0], 10);
+            height = parseInt(dimensions[1], 10);
 
         }
 
@@ -276,7 +276,7 @@
      * if the dfp script has failed to load either through an error or it being blocked by an ad
      * blocker... if it does not load we execute a dummy script to replace the real DFP.
      */
-    var dfpLoader = function() {
+    var dfpLoader = function () {
 
         window.googletag = window.googletag || {};
         window.googletag.cmd = window.googletag.cmd || [];
@@ -286,7 +286,7 @@
         gads.type = 'text/javascript';
 
         // Adblock blocks the load of Ad scripts... so we check for that
-        gads.onerror = function() {
+        gads.onerror = function () {
             dfpBlocked();
         };
 
@@ -297,7 +297,7 @@
         node.parentNode.insertBefore(gads, node);
 
         // Adblock plus seems to hide blocked scripts... so we check for that
-        if(gads.style.display === 'none') {
+        if (gads.style.display === 'none') {
             dfpBlocked();
         }
 
@@ -310,36 +310,36 @@
      * where you are laying DFP over existing content and need to init things like slide shows after the loading
      * is completed.
      */
-    var dfpBlocked = function() {
+    var dfpBlocked = function () {
 
         // Get the stored dfp commands
         var commands = window.googletag.cmd;
 
         // SetTimeout is a bit dirty but the script does not execute in the correct order without it
-        setTimeout(function(){
+        setTimeout(function () {
 
             // overwrite the dfp object - replacing the command array with a function and defining missing functions
             window.googletag = {
-                'cmd': {
-                    'push': function(callback){
+                cmd: {
+                    push: function (callback) {
                         callback.call(dfpScript);
                     }
                 },
-                'ads':[],
-                'pubads':function() {return this;},
-                'enableSingleRequest':function() {return this;},
-                'setTargeting':function() {return this;},
-                'collapseEmptyDivs':function() {return this;},
-                'enableServices':function() {return this;},
-                'defineSlot':function(name,dimensions,id) {
+                ads: [],
+                pubads: function () { return this; },
+                enableSingleRequest: function () { return this; },
+                setTargeting: function () { return this; },
+                collapseEmptyDivs: function () { return this; },
+                enableServices: function () { return this; },
+                defineSlot: function (name, dimensions, id) {
                     window.googletag.ads.push(id);
                     window.googletag.ads[id] = {
-                        'renderEnded':function(){},
-                        'addService':function() {return this;}
+                        renderEnded: function () {},
+                        addService: function () { return this; }
                     };
                     return window.googletag.ads[id];
                 },
-                'display':function(id) {
+                display: function (id) {
                     window.googletag.ads[id].renderEnded.call(dfpScript);
                     return this;
                 }
@@ -347,11 +347,11 @@
             };
 
             // Execute any stored commands
-            $.each(commands,function(k,v){
+            $.each(commands, function (k, v) {
                 window.googletag.cmd.push(v);
             });
 
-        },50);
+        }, 50);
 
     };
 
@@ -360,27 +360,27 @@
      * @param  String id      (Optional) The DFP account ID
      * @param  Object options (Optional) Custom options to apply
      */
-    $.dfp = $.fn.dfp = function(id,options) {
+    $.dfp = $.fn.dfp = function (id, options) {
 
         options = options || {};
 
-        if(typeof id === 'undefined') {
+        if (typeof id === 'undefined') {
             id = dfpID;
         }
 
-        if(typeof id === 'object') {
+        if (typeof id === 'object') {
             options = id;
             id = options.dfpID || dfpID;
         }
 
         var selector = this;
 
-        if(typeof this === 'function') {
+        if (typeof this === 'function') {
             selector = dfpSelector;
         }
 
-        init(id,selector,options);
+        init(id, selector, options);
 
     };
 
-})(window.jQuery,window);
+})(window.jQuery, window);
