7a2d5aa9bd4f6afb71ac265665458a8a67684f59
Matt Cooper <matt@matthewcooper.net>
2014-06-17 18:29:16 +1200

    Added callback tests

---
+++ tests/spec/callBacksSpec.js
@@ -0,0 +1,250 @@
+describe("Callbacks", function () {
+
+    var cleanup = function () {
+        $('.adunit').remove();
+        $('script[src*="gpt.js"]').remove();
+        window.googletag = undefined;
+    };
+
+    beforeEach(cleanup);
+    afterEach(cleanup);
+
+    it("afterEachAdLoaded callback is called twice (two ad units)", function () {
+        var mock = {};
+        mock.slotRenderEnded = function () {};
+        mock.addEventListener = function (event, func) {
+            mock.slotRenderEnded = func;
+        };
+        mock.afterEachAdLoaded = function (param) { };
+
+        spyOn(mock, "afterEachAdLoaded").andCallThrough();
+
+        var dummyTag = {};
+        dummyTag.pubads = function () {
+            return {
+                enableSingleRequest: function () {},
+                setTargeting: function () {},
+                collapseEmptyDivs: function () {},
+                addEventListener: mock.addEventListener
+            };
+        };
+
+        dummyTag.display = function(id) {
+
+            var dummyEvent = {
+                slot: {
+                    getSlotId: function () {
+                        return {
+                            getDomId: function () {
+                                return 'Leader';
+                            }
+                        };
+                    }
+                }
+            };
+
+            mock.slotRenderEnded(dummyEvent);
+        };
+        dummyTag.enableServices = function() {};
+
+        spyOn(dummyTag, "enableServices").andCallThrough();
+
+        runs(function () {
+            $('body').append('<div class="adunit" data-adunit="Leader"></div>');
+            $('body').append('<div class="adunit" data-adunit="Leader"></div>');
+            jQuery.dfp({
+                dfpID: 'xxxxxxxxx',
+                googletag: dummyTag,
+                afterEachAdLoaded: mock.afterEachAdLoaded
+            });
+        }, "Kick off loader");
+
+        waitsFor(function () {
+            if (mock.afterEachAdLoaded.callCount > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }, "Method 'afterEachAdLoaded' never called", 5000);
+
+        runs(function () {
+            expect(mock.afterEachAdLoaded).toHaveBeenCalled();
+            expect(mock.afterEachAdLoaded.callCount).toEqual(2);
+        });
+    });
+
+    it("afterAllAdsLoaded callback is called once (two ad units)", function () {
+        var mock = {};
+        mock.slotRenderEnded = function () {};
+        mock.addEventListener = function (event, func) {
+            mock.slotRenderEnded = func;
+        };
+        mock.afterAllAdsLoaded = function (param) { };
+
+        spyOn(mock, "afterAllAdsLoaded").andCallThrough();
+
+        var dummyTag = {};
+        dummyTag.pubads = function () {
+            return {
+                enableSingleRequest: function () {},
+                setTargeting: function () {},
+                collapseEmptyDivs: function () {},
+                addEventListener: mock.addEventListener
+            };
+        };
+
+        dummyTag.display = function(id) {
+
+            var dummyEvent = {
+                slot: {
+                    getSlotId: function () {
+                        return {
+                            getDomId: function () {
+                                return 'Leader';
+                            }
+                        };
+                    }
+                }
+            };
+
+            mock.slotRenderEnded(dummyEvent);
+        };
+        dummyTag.enableServices = function() {};
+
+        spyOn(dummyTag, "enableServices").andCallThrough();
+
+        runs(function () {
+            $('body').append('<div class="adunit" data-adunit="Leader"></div>');
+            $('body').append('<div class="adunit" data-adunit="Leader"></div>');
+            jQuery.dfp({
+                dfpID: 'xxxxxxxxx',
+                googletag: dummyTag,
+                afterAllAdsLoaded: mock.afterAllAdsLoaded
+            });
+        }, "Kick off loader");
+
+        waitsFor(function () {
+            if (mock.afterAllAdsLoaded.callCount > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }, "Method 'afterEachAdLoaded' never called", 5000);
+
+        runs(function () {
+            expect(mock.afterAllAdsLoaded).toHaveBeenCalled();
+            expect(mock.afterAllAdsLoaded.callCount).toEqual(1);
+        });
+    });
+
+    it("afterAllAdsLoaded callback is called once (single ad unit)", function () {
+        var mock = {};
+        mock.afterAllAdsLoaded = function (param) { };
+
+        spyOn(mock, "afterAllAdsLoaded").andCallThrough();
+
+        var dummyTag = {};
+        dummyTag.display = function () {
+            mock.afterAllAdsLoaded();
+        };
+
+        runs(function () {
+            $('body').append('<div class="adunit" data-adunit="Leader"></div>');
+            jQuery.dfp({
+                dfpID: 'xxxxxxxx',
+                afterAllAdsLoaded: mock.afterAllAdsLoaded,
+                googletag: dummyTag,
+            });
+        }, "Kick off loader");
+
+        waitsFor(function () {
+            if (mock.afterAllAdsLoaded.callCount === 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }, "Method 'afterAllAdsLoaded' never called", 5000);
+
+        runs(function () {
+            expect(mock.afterAllAdsLoaded).toHaveBeenCalled();
+            expect(mock.afterAllAdsLoaded.callCount).toEqual(1);
+        });
+    });
+
+    it("slotRenderEnded event listener is added", function () {
+        var mock = {};
+        mock.addEventListener = function (param) { };
+
+        var dummyTag = {};
+        dummyTag.pubads = function () {
+            return {
+                enableSingleRequest: function () {},
+                setTargeting: function () {},
+                collapseEmptyDivs: function () {},
+                addEventListener: mock.addEventListener
+            };
+        };
+
+        dummyTag.enableServices = function() {};
+
+        spyOn(dummyTag, "enableServices").andCallThrough();
+        spyOn(mock, "addEventListener").andCallThrough();
+
+        runs(function () {
+            jQuery.dfp({
+                dfpID: 'xxxxxxxxx',
+                googletag: dummyTag
+            });
+        }, "Kick off loader");
+
+        waitsFor(function () {
+            if (dummyTag.enableServices.callCount === 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }, "Method 'enableServices' never called", 5000);
+
+        runs(function () {
+            expect(mock.addEventListener).toHaveBeenCalled();
+            expect(mock.addEventListener.callCount).toEqual(1);
+            expect(mock.addEventListener.calls[0].args.length).toEqual(2);
+            expect(mock.addEventListener.calls[0].args[0]).toEqual('slotRenderEnded');
+        });
+    });
+
+    it("afterEachAdLoaded callback is called once (single ad unit)", function () {
+        var mock = {};
+        mock.afterEachAdLoaded = function (param) { };
+
+        spyOn(mock, "afterEachAdLoaded").andCallThrough();
+
+        var dummyTag = {};
+        dummyTag.display = function () {
+            mock.afterEachAdLoaded();
+        };
+
+        runs(function () {
+            $('body').append('<div class="adunit" data-adunit="Leader"></div>');
+            jQuery.dfp({
+                dfpID: 'xxxxxxxx',
+                afterEachAdLoaded: mock.afterEachAdLoaded,
+                googletag: dummyTag,
+            });
+        }, "Kick off loader");
+
+        waitsFor(function () {
+            if (mock.afterEachAdLoaded.callCount === 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }, "Method 'afterEachAdLoaded' never called", 5000);
+
+        runs(function () {
+            expect(mock.afterEachAdLoaded).toHaveBeenCalled();
+            expect(mock.afterEachAdLoaded.callCount).toEqual(1);
+        });
+    });
+
+});
